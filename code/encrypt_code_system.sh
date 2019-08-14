8b036a8745480e6e48377df0f6901cfbdfd4c32a
root <nguyenduytamgithub@gmail.com>
2019-08-14 08:14:09 +0100

    update

--- code/encrypt_code_system.sh
+++ code/encrypt_code_system.sh
@@ -1,1 +1,1 @@
-openssl enc -aes-256-cbc -base64 -in /home/pi/identify_box/file/data_full -out /home/pi/identify_box/file/encrypt_packet_2 -k /home/pi/identify_box/file/code_encrypt
+openssl enc -base64 -in /home/pi/identify_box/file/data_full -out /home/pi/identify_box/file/encrypt_packet_2 -k /home/pi/identify_box/file/code_encrypt
